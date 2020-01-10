d7356518ee7f59779358421df94238da4c97c994
Vincent Schoettke <vincentschoettke@gmail.com>
2020-01-10 21:53:03 +0100

    fix new lint errors

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ var lohnsteuerFuncs = require("./lohnsteuer");
 
 function resultsToNumbers(results) {
     return Object.keys(results).reduce(function (res, key) {
-        res[key] = +String(results[key]);
+        res[key] = Number(String(results[key]));
         return res;
     }, {});
 }
@@ -18,8 +18,7 @@ function resultsToStrings(results) {
 }
 
 function chain(a, b) {
-    return function () {
-        var args = Array.prototype.slice.call(arguments);
+    return function (...args) {
         return a(b.apply(undefined, args));
     };
 }
